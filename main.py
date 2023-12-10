229411bdf263a7bef908ed82d5595785b7b34526
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-12-10 08:47:01 +0100

    Refactor choose_video_file function to handle exceptions

--- main.py
+++ main.py
@@ -25,45 +25,52 @@ window.rowconfigure([0, 1], weight=1)
 window.columnconfigure([0, 1, 2, 3, 4, 5], weight=1)
 
 def choose_video_file():
-    # get the path of the video file
-    video_path = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select a video file", filetypes=[("Video files", "*.mp4;*.avi;*.mkv")])
-    # get the name of the video file without the extension
-    video_name = os.path.splitext(os.path.basename(video_path))[0]
-    # create a video capture object
-    cap = cv2.VideoCapture(video_path)
-    # fix the width and height of the video to 640 and 480
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    # set the frame rate to 30
-    cap.set(cv2.CAP_PROP_FPS, 30)
+    try:
+        # get the path of the video file
+        video_path = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select a video file", filetypes=[("Video files", "*.mp4;*.avi;*.mkv")])
+        # get the name of the video file without the extension
+        video_name = os.path.splitext(os.path.basename(video_path))[0]
+        # create a video capture object
+        cap = cv2.VideoCapture(video_path)
+        # fix the width and height of the video to 640 and 480
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        # set the frame rate to 30
+        cap.set(cv2.CAP_PROP_FPS, 30)
 
-    hand_speed_tracker = hst.HandSpeedTracker()
-    speed_list = {
-        "left_hand": [],
-        "right_hand": []
-    }
-    while cap.isOpened():
-        success, image = cap.read() # reading the image from the webcam
-        if not success:
-            print("Ignoring empty video frame.")
-            continue
-        
-        image, coordinates = hand_speed_tracker.find_hands(image)
-        left_hand_speed = hand_speed_tracker.calculate_speed(coordinates["left_hand"])
-        right_hand_speed = hand_speed_tracker.calculate_speed(coordinates["right_hand"])
-        speed_list["left_hand"].append(left_hand_speed)
-        speed_list["right_hand"].append(right_hand_speed)
-        cv2.imshow("Hand Tracker", image)
+        hand_speed_tracker = hst.HandSpeedTracker()
+        speed_list = {
+            "left_hand": [],
+            "right_hand": []
+        }
+        while cap.isOpened():
+            success, image = cap.read() # reading the image from the webcam
+            if not success:
+                print("Ignoring empty video frame.")
+                continue
+            
+            image, coordinates = hand_speed_tracker.find_hands(image)
+            left_hand_speed = hand_speed_tracker.calculate_speed(coordinates["left_hand"])
+            right_hand_speed = hand_speed_tracker.calculate_speed(coordinates["right_hand"])
+            speed_list["left_hand"].append(left_hand_speed)
+            speed_list["right_hand"].append(right_hand_speed)
+            cv2.imshow("Hand Tracker", image)
 
-        key = cv2.waitKey(1)
-        if key == ord('s'):
-            hand_speed_tracker.plot_speed(speed_list["left_hand"], "left", video_name)
-            hand_speed_tracker.plot_speed(speed_list["right_hand"], "right", video_name)
-            hand_speed_tracker.save_results(video_name + ".xlsx", coordinates, speed_list)
-            hand_speed_tracker.close()
-            cap.release()
-            cv2.destroyAllWindows()
-            break
+            key = cv2.waitKey(1)
+            if key == ord('s'):
+                hand_speed_tracker.plot_speed(speed_list["left_hand"], "left", video_name)
+                hand_speed_tracker.plot_speed(speed_list["right_hand"], "right", video_name)
+                hand_speed_tracker.save_results(video_name + ".xlsx", coordinates, speed_list)
+                hand_speed_tracker.close()
+                cap.release()
+                cv2.destroyAllWindows()
+                break
+    except:
+        # Display an error message in the window
+        label_info.configure(text="Something went wrong!", fg="red", font=("Arial", 16))
+        # Quit the window after 1 seconds
+        window.after(1000, quit)
+
 
 def quit():
     window.quit()
